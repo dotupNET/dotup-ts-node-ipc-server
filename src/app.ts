5d5735b14df5b2af21c667688d10a275ce620201
Peter Ullrich <dotup.software@gmail.com>
2020-03-11 12:18:34 +0100

    lint

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ const nameOrPort = process.env.IPC_CHANNEL || "DOTUP-IPC-SERVER";
 
 // vars
 let prefix = "production";
-let logger: Log4js;
+let logger: Log4js | undefined;
 
 if (process.env.NODE_ENV !== undefined) {
   prefix = process.env.NODE_ENV;
@@ -29,6 +29,7 @@ const initialize = async () => {
     const exitApp = () => {
       ipcServer?.stop();
       logger?.shutdown(e => console.log(e));
+      logger = undefined;
       clearInterval(timer);
       process.off("SIGINT", exitApp);
       process.off("SIGTERM", exitApp);
